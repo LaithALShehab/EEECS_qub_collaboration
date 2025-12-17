fb5ace68c96e57bc35e0307d084ab7b86d5aadd2
Laith Waleed Ahmad Al shehab <Laith Waleed AL Shehab>
2025-12-17 12:17:42 +0000

    creates flexible add_two_num.py

--- add_two_nums.py
+++ add_two_nums.py
@@ -1,4 +1,21 @@
-def add_two_nums(num1, num2):
-  return num1+num2
+import sys
+
+
+def add_nums(args):
+	total=0
+	for num in args:
+		total+=int(num)
+	return total
+
+
+
+def main():
+	scriptname=sys.argv[0]
+	result = add_nums(sys.argv[1:])
+	print("The script name is : ", scriptname)
+	print("Total:", result)
+
+if __name__ == "__main__":
+	main()
 
 # TODO add print statement
